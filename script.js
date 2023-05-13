66c58ff0da3704cadb5ea989f633d2dacde0a8fb
Omkarnath Panage <118303129+Omkarnath1@users.noreply.github.com>
2023-05-13 22:52:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,46 +7,46 @@ let arr = [
 ];
 
 function PrintStudentswithMap() {
-  //Write your code here , just console.log
-      arr.map((arr)=>
-    console.log(arr)
+    //Write your code here , just console.log
+    arr.map((arr)=>{
+    if(arr.marks>50)
+    console.log(arr)}
     )
-}
-
-function PrintStudentsbyForEach() {
-  //Write your code here , just console.log
-    arr.forEach(element => {
-        console.log(element)
-    });
-}
-
-function addData() {
-  //Write your code here, just console.log
-        let newStudent = {
-        id: 4,
-        name: "joe", 
-        age: "22", 
-        marks: 95
+  }
+  
+  function PrintStudentsbyForEach() {
+    //Write your code here , just console.log
+   arr.forEach((arr)=>{
+    if(arr.marks>50){
+        console.log(arr)
+    }
+   })
+  }
+  
+  function addData() {
+    //Write your code here, just console.log
+    let newStudent = {
+        id:4,name:"susan",age:"20",marks:45
     }
     arr.push(newStudent);
     console.log(arr);
-}
-
-function removeFailedStudent() {
-  //Write your code here, just console.log
-  let newArr =  arr.filter((arr)=>{
-          return arr.marks>35;
+  }
+  
+  function removeFailedStudent() {
+    //Write your code here, just console.log
+   let newArr =  arr.filter((arr)=>{
+          return arr.marks>=50;
         }
     )
     arr = newArr;
     console.log(arr);
-}
-
-function concatenateArray() {
-  //Write your code here, just console.log
-      let newArr = [ { id: 5, name: "jack", age: "19", marks: 50 },
-    { id: 2, name: "josef", age: "29", marks: 65 },
-    { id: 3, name: "karly", age: "19", marks: 43 },];
+  }
+  
+  function concatenateArray() {
+    //Write your code here, just console.log
+    let newArr = [ { id: 5, name: "jack", age: "19", marks: 50 },
+    { id: 6, name: "josef", age: "29", marks: 65 },
+    { id: 7, name: "karly", age: "19", marks: 43 },];
     
     console.log(arr.concat(newArr));
-}
+  }
